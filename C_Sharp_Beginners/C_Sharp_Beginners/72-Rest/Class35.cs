09d519f2694effb4c94c43560a9fbe7448e0a82e
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2023-03-24 11:14:48 +0600

    88

--- C_Sharp_Beginners/C_Sharp_Beginners/72-Rest/Class35.cs
+++ C_Sharp_Beginners/C_Sharp_Beginners/72-Rest/Class35.cs
@@ -1,91 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-//Part 78 Sort a list of complex types in c#
+////Part 78 Sort a list of complex types in c#
 
 
-namespace C_Sharp_Begginners._72_Rest
-{
-    internal class Class35
-    {
-        public static void Main()
-        {
-             Customer customer1 = new Customer()
-        {
-            ID = 101,
-            Name = "Mark",
-            Salary = 4000
-        };
+//namespace C_Sharp_Begginners._72_Rest
+//{
+//    internal class Class35
+//    {
+//        public static void Main()
+//        {
+//             Customer customer1 = new Customer()
+//        {
+//            ID = 101,
+//            Name = "Mark",
+//            Salary = 4000
+//        };
 
-        Customer customer2 = new Customer()
-        {
-            ID = 102,
-            Name = "John",
-            Salary = 7000
-        };
+//        Customer customer2 = new Customer()
+//        {
+//            ID = 102,
+//            Name = "John",
+//            Salary = 7000
+//        };
 
-        Customer customer3 = new Customer()
-        {
-            ID = 103,
-            Name = "Ken",
-            Salary = 5500
-        };
+//        Customer customer3 = new Customer()
+//        {
+//            ID = 103,
+//            Name = "Ken",
+//            Salary = 5500
+//        };
 
-        List<Customer> listCutomers = new List<Customer>();
-        listCutomers.Add(customer1);
-        listCutomers.Add(customer2);
-        listCutomers.Add(customer3);
+//        List<Customer> listCutomers = new List<Customer>();
+//        listCutomers.Add(customer1);
+//        listCutomers.Add(customer2);
+//        listCutomers.Add(customer3);
 
-        Console.WriteLine("Customers before sorting");
-        foreach (Customer customer in listCutomers)
-        {
-            Console.WriteLine(customer.Name + " - " + customer.Salary);
-        }
+//        Console.WriteLine("Customers before sorting");
+//        foreach (Customer customer in listCutomers)
+//        {
+//            Console.WriteLine(customer.Name + " - " + customer.Salary);
+//        }
 
-        // Sort() method should sort customers by salary
-        listCutomers.Sort();
+//        // Sort() method should sort customers by salary
+//        listCutomers.Sort();
 
-        Console.WriteLine("Customers after sorting");
-        foreach (Customer customer in listCutomers)
-        {
-            Console.WriteLine(customer.Name + " - " + customer.Salary);
-        }
+//        Console.WriteLine("Customers after sorting");
+//        foreach (Customer customer in listCutomers)
+//        {
+//            Console.WriteLine(customer.Name + " - " + customer.Salary);
+//        }
 
-        // To sort customers by name instead of salary
-        SortByName sortByName = new SortByName();
-        listCutomers.Sort(sortByName);
+//        // To sort customers by name instead of salary
+//        SortByName sortByName = new SortByName();
+//        listCutomers.Sort(sortByName);
 
-        Console.WriteLine("Customers after sorting by Name");
-        foreach (Customer customer in listCutomers)
-        {
-            Console.WriteLine(customer.Name + " - " + customer.Salary);
-        }
+//        Console.WriteLine("Customers after sorting by Name");
+//        foreach (Customer customer in listCutomers)
+//        {
+//            Console.WriteLine(customer.Name + " - " + customer.Salary);
+//        }
 
-        Console.ReadLine();
-    }
-    }
+//        Console.ReadLine();
+//    }
+//    }
 
-   public class SortByName : IComparer<Customer>
-{
-    public int Compare(Customer x, Customer y)
-    {
-        return x.Name.CompareTo(y.Name);
-    }
-}
+//   public class SortByName : IComparer<Customer>
+//{
+//    public int Compare(Customer x, Customer y)
+//    {
+//        return x.Name.CompareTo(y.Name);
+//    }
+//}
 
-    public class Customer:IComparable<Customer>
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public int Salary { get; set; }
-        public string Type { get; set; }
+//    public class Customer:IComparable<Customer>
+//    {
+//        public int ID { get; set; }
+//        public string Name { get; set; }
+//        public int Salary { get; set; }
+//        public string Type { get; set; }
 
-        public int CompareTo(Customer other)
-        {
-            return this.Salary.CompareTo(other.Salary);
-        }
-    }
-}
+//        public int CompareTo(Customer other)
+//        {
+//            return this.Salary.CompareTo(other.Salary);
+//        }
+//    }
+//}
---
+++ C_Sharp_Beginners/C_Sharp_Beginners/72-Rest/Class37.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Beginners._72_Rest
+{
+    //Part 88 - ThreadStart delegate
+    class Class37
+    {
+        public static void Main()
+        {
+            //Thread t1 = new Thread(Number.PrintNumbers);
+
+            //We can rewrite the above line using ThreadStart delegate as shown below.
+            Thread t1 = new Thread(new ThreadStart(Number.PrintNumbers));
+            t1.Start();
+
+            //We can also rewrite the same line using delagate() keyword as shown below.
+            Thread t2 = new Thread(delegate () { Number.PrintNumbers(); });
+            t2.Start();
+        }
+    }
+
+    class Number
+    {
+        public static void PrintNumbers()
+        {
+            for(int i=0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
